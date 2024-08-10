2b330d1f3a44ef724e2599d96059e4d5565c424b
kiwiphoenix <kiwiphoenix364@gmail.com>
2024-08-09 23:36:40 -0400

    Update tags.js

--- kubejs/server_scripts/tags.js
+++ kubejs/server_scripts/tags.js
@@ -1,4 +1,4 @@
 ServerEvents.tags('item', event => {
     event.add('minecraft:logs', '#endlessexpansion:arbor_logs')
-    event.add('notreepunching:weak_saws', ['endlessexpansion:cobalt_axe','endlessexpansion:celestial_axe','endlessexpansion:shadowsteel_axe','aquaculture:neptunium_axe','iceandfire:silver_axe','iceandfire:copper_axe','iceandfire:dragonbone_axe','iceandfire:myrmex_desert_axe','iceandfire:myrmex_jungle_axe','iceandfire:dragonsteel_fire_axe','iceandfire:dragonsteel_ice_axe','iceandfire:dragonsteel_lightning_axe'])
+    event.add('notreepunching:weak_saws', ['minecraft:wooden_axe','minecraft:stone_axe','endlessexpansion:cobalt_axe','endlessexpansion:celestial_axe','endlessexpansion:shadowsteel_axe','aquaculture:neptunium_axe','iceandfire:silver_axe','iceandfire:copper_axe','iceandfire:dragonbone_axe','iceandfire:myrmex_desert_axe','iceandfire:myrmex_jungle_axe','iceandfire:dragonsteel_fire_axe','iceandfire:dragonsteel_ice_axe','iceandfire:dragonsteel_lightning_axe'])
 })
